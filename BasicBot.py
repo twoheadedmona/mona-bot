46063ecb85490d3a8f1d804f69abdfec498e11a3
Monika Jovanova <monika@netbit.local>
2018-01-29 16:30:24 +0100

    Bot can now play music

--- BasicBot.py
+++ BasicBot.py
@@ -8,6 +8,8 @@ import random
 import safygiphy
 import requests
 import io
+import youtube_dl
+
 
 # Here you can modify the bot's prefix and description and wether it sends help in direct messages or not.
 client = Bot(description="Basic Bot",
@@ -15,6 +17,9 @@ client = Bot(description="Basic Bot",
 
 giphy = safygiphy.Giphy()
 players = {}
+
+choicemsgid = None
+msguser = None
 # This is what happens everytime the bot launches. In this case, it prints information like server count, user count the bot is connected to, and the bot id in the console.
 # Do not mess with it because the bot can break, if you wish to do so, please consult me or someone trusted.
 
@@ -34,6 +39,15 @@ async def on_message(message):
     member = message.author
     emb = discord.Embed(title="Tile", description="Desc", color=0x00ff00)
     if message.content.startswith('!test'):
+        ydl_opts = {
+            'forcedesctiption': True,
+            'default_search': "ytsearch3"
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            meta = ydl.extract_info("cat", download=False)
+        ytentries = meta['entries']
+        for entry in ytentries:
+            await client.send_message(message.channel, entry['title'])
         await client.send_message(member, 'Test')
 
     if message.content.startswith('!hi'):
@@ -53,8 +67,8 @@ async def on_message(message):
         if message.content.endswith("?"):
             if msg[:2] == 'wh' or msg[0] == 'h':
                 await client.send_message(message.channel, embed=discord.Embed(
-                    title = ballsays + "I'm sorry, I can't answer that",
-                    description ="Ask me a yes or no question, and make sure you use **?** ",
+                    title=ballsays + "I'm sorry, I can't answer that",
+                    description="Ask me a yes or no question, and make sure you use **?** ",
                     color=0x0099ff
                 ))
             else:
@@ -78,43 +92,85 @@ async def on_message(message):
                            "My sources say no :8ball:",
                            "Outlook not so good :8ball:",
                            "Very doubtful :8ball:")
-                embed = discord.Embed(title= ballsays,
+                embed = discord.Embed(title=ballsays,
                                       description=random.choice(answers),
                                       color=0x0099ff)
                 await client.send_message(message.channel, embed=embed)
         else:
             await client.send_message(message.channel, embed=discord.Embed(
-                title = ballsays + "Ask me a question",
-                description = "And make sure you use **?**",
-                color = 0x0099ff
+                title=ballsays + "Ask me a question",
+                description="And make sure you use **?**",
+                color=0x0099ff
             ))
-    
+
     if message.content.startswith("!coin"):
-        choice = random.randint(1,2)
+        choice = random.randint(1, 2)
         if choice == 1:
             await client.add_reaction(message, "\U000026AA")
         else:
             await client.add_reaction(message, "\U000026AB")
 
-            
     if message.content.startswith("!gif"):
         gif_tag = message.content.replace("!gif", "").lstrip().rstrip()
-        rgif = giphy.random(tag = str(gif_tag))
+        rgif = giphy.random(tag=str(gif_tag))
         response = requests.get(
             str(rgif.get("data", {}).get('image_original_url')), stream=True
         )
         await client.send_file(message.channel, io.BytesIO(response.raw.read()), filename='video.gif')
 
     if message.content.startswith('!play'):
-
         # yt_url = message.content[6:]
         link = message.content.replace("!play", "").lstrip().rstrip()
-        channel = message.author.voice.voice_channel
-        voice = await client.join_voice_channel(channel)
-        player = await voice.create_ytdl_player(link)
-        players[message.server.id] = player
-        player.start()
+        if link.startswith("https"):
+            channel = message.author.voice.voice_channel
+            voice = await client.join_voice_channel(channel)
+            player = await voice.create_ytdl_player(link)
+            await client.send_message(message.channel, "Now playing: " + player.title)
+            players[message.server.id] = player
+            player.start()
+        else:
+            pairs = {}
+            ydl_opts = {
+                'forcedesctiption': True,
+                'default_search': "ytsearch3"
+            }
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                meta = ydl.extract_info(link, download=False)
+            ytentries = meta['entries']
+            for index, entry in enumerate(ytentries):
+                msg = await client.send_message(message.channel, "{0}. {1}".format(index+1, entry['title']))
+                pairs.update({index+1 : entry['webpage_url']})
+            
+            await client.add_reaction(msg, '\N{DIGIT ONE}\N{COMBINING ENCLOSING KEYCAP}')
+            await client.add_reaction(msg, '\N{DIGIT TWO}\N{COMBINING ENCLOSING KEYCAP}')
+            await client.add_reaction(msg, '\N{DIGIT THREE}\N{COMBINING ENCLOSING KEYCAP}')
 
+            res = await client.wait_for_reaction(['\N{DIGIT ONE}\N{COMBINING ENCLOSING KEYCAP}', '\N{DIGIT TWO}\N{COMBINING ENCLOSING KEYCAP}', '\N{DIGIT THREE}\N{COMBINING ENCLOSING KEYCAP}'], message=msg, user=message.author, timeout = 20.0)
+            # await client.send_message(message.channel, '{0.user} reacted with {0.reaction.emoji}!'.format(res))
+            
+            channel = message.author.voice.voice_channel
+            voice = await client.join_voice_channel(channel)
+            player = None
+            if res.user == message.author and res.reaction.message.id == msg.id:
+                print('hello monika this is the right message')
+                if res.reaction.emoji == '\N{DIGIT ONE}\N{COMBINING ENCLOSING KEYCAP}':
+                    player = await voice.create_ytdl_player(pairs[1])
+
+                if res.reaction.emoji == '\N{DIGIT TWO}\N{COMBINING ENCLOSING KEYCAP}':
+                    player = await voice.create_ytdl_player(pairs[2])
+                
+                if res.reaction.emoji == '\N{DIGIT THREE}\N{COMBINING ENCLOSING KEYCAP}':
+                    player = await voice.create_ytdl_player(pairs[3])
+
+            await client.send_message(message.channel, "Now playing: {0.title} \n {0.url}".format(player))
+            pairs.clear()
+            player.start()
+            # global choicemsgid
+            # choicemsgid = msg.id
+
+            # global msguser
+            # msguser = message.author
+            
     if message.content.startswith('!quit'):
         try:
             voice_client = client.voice_client_in(message.server)
@@ -123,14 +179,26 @@ async def on_message(message):
             await client.send_message(message.channel, "no.")
         except Exception as Hugo:
             await client.send_message(message.channel, "Ein Error: ```{haus}```".format(haus=Hugo))
-            
 
+    await client.process_commands(message)
+
+
+# @client.event
+# async def on_reaction_add(reaction, user):
+#     msg = reaction.message
+#     channel = reaction.message.channel
+
+#     if msg.id == choicemsgid and user == msguser:
+#         if reaction.emoji == '\N{DIGIT ONE}\N{COMBINING ENCLOSING KEYCAP}':
+#             print('hello')
+        
 
 @client.event
 async def on_member_join(member):
     server = member.server
     fmt = 'Welcome {0.mention} to {1.name}!'
     await client.send_message(server, fmt.format(member, server))
+
 
 # This is a basic example of a call and response command. You tell it do "this" and it does it.
 
